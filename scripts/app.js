bed41046447138805031a97eeee78ae53cafbf08
maccana <mikeymccann@gmail.com>
2016-08-03 12:48:33 -0400

    Update app.js to save selected cities to local storage

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,32 @@
   'use strict';
 
   // Insert injected weather forecast here
+  var initialWeatherForecast = {  
+    key: 'newyork',  
+    label: 'New York, NY',  
+    currently: {  
+      time: 1453489481,  
+      summary: 'Clear',  
+      icon: 'partly-cloudy-day',  
+      temperature: 52.74,  
+      apparentTemperature: 74.34,  
+      precipProbability: 0.20,  
+      humidity: 0.77,  
+      windBearing: 125,  
+      windSpeed: 1.52  
+    },  
+    daily: {  
+      data: [  
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},  
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}  
+      ]  
+    }  
+  };
 
 
   var app = {
@@ -180,11 +206,45 @@
     }
   };
   // Uncomment the line below to test the app with fake data
-   app.updateForecastCard(fakeForecast);
+  // app.updateForecastCard(fakeForecast);
 
   // Add code to save the users list of subscribed cities here
 
   // Add code to check if the user has any subscribed cities, and render 
   // those or the default data here.
+
+
+      // Save list of cities to localStorage, see note below about localStorage.
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    // IMPORTANT: See notes about use of localStorage.
+    localStorage.selectedCities = selectedCities;
+  };
+
+/****************************************************************************   
+ *
+ * Code required to start the app
+ *
+ * NOTE: To simplify this getting started guide, we've used localStorage.
+ *   localStorage is a synchronous API and has serious performance
+ *   implications. It should not be used in production applications!
+ *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+ *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+ *
+ ****************************************************************************/
+
+app.selectedCities = localStorage.selectedCities;
+if (app.selectedCities) {
+  app.selectedCities = JSON.parse(app.selectedCities);
+  app.selectedCities.forEach(function(city) {
+    app.getForecast(city.key, city.label);
+  });
+} else {
+  app.updateForecastCard(initialWeatherForecast);
+  app.selectedCities = [
+    {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+  ];
+  app.saveSelectedCities();
+}
 
 })();
